fd1c25485b22057cff9cc62f67a3ed92449dade9
SkylarJudd <sjudd1@myjmc.edu.au>
2023-07-11 12:45:02 +1000

    Tri 1 Week 5 RollerBall
    Added Random spawns to prefabs

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -12,7 +12,7 @@ public class CubeSpawner : MonoBehaviour
     void Start()
     {
        
-        CubeAmout = Random.Range(4, 10);
+        CubeAmout = Random.Range(1, 4);
         SpawnCube();
         print("CubeAmout " + CubeAmout);
     }
@@ -23,7 +23,8 @@ public class CubeSpawner : MonoBehaviour
         for (int i = 0; i < CubeAmout; i++)
         {
             GameObject go = Instantiate(Cube);
-            go.transform.position = new Vector3(Random.Range(-9, 9), 1, Random.Range(-9, 9));
+            //go.transform.localPosition = new Vector3( transform.localPosition.x+30, 1 , transform.localPosition.z - 30);
+            go.transform.localPosition = new Vector3(Random.Range(transform.localPosition.x, transform.localPosition.x+30), 1, Random.Range(transform.localPosition.z, transform.localPosition.z - 30));
         }
        // GameObject go = Instantiate(Cube);
       // go.transform.position = new Vector3(Random.Range(-10,10), 1 , Random.Range(-10, 10));
