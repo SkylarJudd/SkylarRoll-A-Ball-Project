b84a93ec9be9701aac58427d0f46bd97f5ecab04
SkylarJudd <136395161+SkylarJudd@users.noreply.github.com>
2023-08-02 18:35:12 +1000

    Tri 1 - Week 9 - imported meshes to unity

--- Assets/Scripts/Roatator.cs
+++ Assets/Scripts/Roatator.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Roatator : MonoBehaviour
 {
-    public float Speed = 1f;
+    public float Speed = 20f;
     public bool up = true;
-    public float ToggleCount = 1;
+    public float Amplitude = 1;
+    public float frequency = 1;
+    public Vector3 StartingPosition;
 
 
 
@@ -14,6 +16,8 @@ public class Roatator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Speed = Random.Range(40, 80);
+        StartingPosition = transform.position;
 
     }
 
@@ -21,8 +25,9 @@ public class Roatator : MonoBehaviour
     void Update()
     {
         //rotates the cubes a random amout 
-        transform.Rotate(new Vector3(Random.Range(1, 2), Random.Range(1, 2), Random.Range(1, 2) * Time.deltaTime * Speed));
+        transform.Rotate(Vector3.up * Time.deltaTime * Speed);
 
-       
+        transform.position = new Vector3(StartingPosition.x, Mathf.Sin(Time.time * frequency) * Amplitude + StartingPosition.y, StartingPosition.z);
+
     }
 }
